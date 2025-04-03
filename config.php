25d8823cbae8c5dd82a7b0cfadee74e14b6760df
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-04-03 08:09:08 +0530

    add config file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$server="localhost";
+$db_username="root";
+$db_password="asr@712jmn100";
+$db_name="home_products";
+
+$con=mysqli_connect($server,$db_username,$db_password,$db_name);
+if($con->connect_error){
+    die("database connection error");
+}else {
+    echo "";
+}
+?>
